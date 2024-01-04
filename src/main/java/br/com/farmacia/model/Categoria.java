8c815706fce2cac44d9d637addcaef76cd0cb10d
Vinicius Lima <vinicius.slima99@icloud.com>
2024-01-04 11:35:53 -0300

    Criando as Classes Categoria (Model,Repository e Controller)

---
+++ src/main/java/br/com/farmacia/model/Categoria.java
@@ -0,0 +1,39 @@
+package br.com.farmacia.model;
+
+import jakarta.persistence.Entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull(message = "O atributo tipo é obrigatório!")
+	private String tipo;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+
+}
