5f1d274c619aed9380163821cb69928235eebcb2
Vinicius Lima <vinicius.slima99@icloud.com>
2024-01-04 12:55:53 -0300

    Criando o CRUD da Classe Produto

--- src/main/java/br/com/farmacia/model/Categoria.java
+++ src/main/java/br/com/farmacia/model/Categoria.java
@@ -1,10 +1,18 @@
 package br.com.farmacia.model;
 
-import jakarta.persistence.Entity;
+import java.util.List;
 
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import br.com.farmacia.model.Produto;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 
@@ -18,6 +26,10 @@ public class Categoria {
 	
 	@NotNull(message = "O atributo tipo é obrigatório!")
 	private String tipo;
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties
+	private List<Produto> produto;
 
 	public Long getId() {
 		return id;
@@ -33,6 +45,14 @@ public class Categoria {
 
 	public void setTipo(String tipo) {
 		this.tipo = tipo;
+	}
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
 	}
 
 
